8f8145e2c4b9e26a5a3a963f341bcf5b21d62a9d
Giant-Developer <tech@ultracombos.com>
2022-10-21 15:36:05 +0800

    add spout layout

--- Assets/_Scripts/Controller/OSCController.cs
+++ Assets/_Scripts/Controller/OSCController.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 
 public class OSCController : ControlBase
 {
-
+    
     public void SetDancerPos(Vector2 v2)
     {
+        if (dancer == null)
+            return;
         dancer.transform.position = v2;
     }
 
